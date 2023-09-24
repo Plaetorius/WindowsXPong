abadc08243d6356cad7ce81cc4e1e7a54e88781d
Tom GERNEZ <tgernez@student.42.fr>
2023-09-24 05:55:10 +0200

    add windows everywhere

--- script.js
+++ script.js
@@ -123,3 +123,115 @@ let titleBarFriends = document.querySelector(".window-friends-titlebar");
 titleBarFriends.addEventListener("mousedown", startDragFriends);
 friendsWindow.addEventListener("mouseup", stopDragFriends);
 friendsWindow.addEventListener("mousemove", dragFriends);
+
+/* MESSAGES WINDOW */
+function messages_button() {
+    const button = document.getElementById('openMessages');
+
+    if (button.classList.contains('program')) {
+        button.classList.remove('program');
+        button.classList.add('program-pressed');
+    } else {
+        button.classList.remove('program-pressed');
+        button.classList.add('program');
+    }
+}
+
+
+document.getElementById("openMessages").addEventListener("click", function() {
+    document.getElementById("messagesWindow").style.display = "block";
+});
+
+function closeMessages() {
+    document.getElementById("messagesWindow").style.display = "none";
+    const button = document.getElementById('openMessages');
+
+    if (button.classList.contains('program-pressed')) {
+        button.classList.remove('program-pressed');
+        button.classList.add('program');
+    }
+}
+
+let isDraggingMessages = false;
+let offsetXMessages, offsetYMessages;
+
+function startDragMessages(e) {
+    isDraggingMessages = true;
+    offsetXMessages = e.clientX - messagesWindow.getBoundingClientRect().left;
+    offsetYMessages = e.clientY - messagesWindow.getBoundingClientRect().top;
+}
+
+function stopDragMessages() {
+    isDraggingMessages = false;
+}
+
+function dragMessages(e) {
+    if (!isDraggingMessages) return;
+    let x = e.clientX - offsetXMessages;
+    let y = e.clientY - offsetYMessages;
+    messagesWindow.style.left = x + 'px';
+    messagesWindow.style.top = y + 'px';
+}
+
+let messagesWindow = document.getElementById("messagesWindow");
+let titleBarMessages = document.querySelector(".window-messages-titlebar");
+
+titleBarMessages.addEventListener("mousedown", startDragMessages);
+messagesWindow.addEventListener("mouseup", stopDragMessages);
+messagesWindow.addEventListener("mousemove", dragMessages);
+
+/* PINBALL WINDOW */
+function pinball_button() {
+    const button = document.getElementById('openPinball');
+
+    if (button.classList.contains('program')) {
+        button.classList.remove('program');
+        button.classList.add('program-pressed');
+    } else {
+        button.classList.remove('program-pressed');
+        button.classList.add('program');
+    }
+}
+
+
+document.getElementById("openPinball").addEventListener("click", function() {
+    document.getElementById("pinballWindow").style.display = "block";
+});
+
+function closePinball() {
+    document.getElementById("pinballWindow").style.display = "none";
+    const button = document.getElementById('openPinball');
+
+    if (button.classList.contains('program-pressed')) {
+        button.classList.remove('program-pressed');
+        button.classList.add('program');
+    }
+}
+
+let isDraggingPinball = false;
+let offsetXPinball, offsetYPinball;
+
+function startDragPinball(e) {
+    isDraggingPinball = true;
+    offsetXPinball = e.clientX - pinballWindow.getBoundingClientRect().left;
+    offsetYPinball = e.clientY - pinballWindow.getBoundingClientRect().top;
+}
+
+function stopDragPinball() {
+    isDraggingPinball = false;
+}
+
+function dragPinball(e) {
+    if (!isDraggingPinball) return;
+    let x = e.clientX - offsetXPinball;
+    let y = e.clientY - offsetYPinball;
+    pinballWindow.style.left = x + 'px';
+    pinballWindow.style.top = y + 'px';
+}
+
+let pinballWindow = document.getElementById("pinballWindow");
+let titleBarPinball = document.querySelector(".window-pinball-titlebar");
+
+titleBarPinball.addEventListener("mousedown", startDragPinball);
+pinballWindow.addEventListener("mouseup", stopDragPinball);
+pinballWindow.addEventListener("mousemove", dragPinball);
